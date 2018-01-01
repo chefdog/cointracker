2eb07a77d0071c88034f0ad9e1fc12499213abac
Marco van Zuijlen <marco@chefdog.com>
2018-01-01 20:56:15 +0100

    added httpclient handler

---
+++ CryptoTracker.WebApi/CryptoTracker.DataAccess/Models/ExchangeModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace CryptoTracker.DataAccess.Models
+{
+    [DataContract]
+    public class ExchangeModel
+    {
+        [DataMember(Name ="id")]
+        public string Title { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        [DataMember(Name = "symbol")]
+        public string Tag { get; set; }
+
+        [DataMember(Name = "rank")]
+        public string Rank { get; set; }
+
+        [DataMember(Name = "price_usd")]
+        public decimal UsdPrice { get; set; }
+
+        [DataMember(Name = "price_btc")]
+        public string BtcPrice { get; set; }
+
+        [DataMember(Name = "24_volume_usd")]
+        public string Volume { get; set; }
+
+        [DataMember(Name = "market_cap_usd")]
+        public string MarketCapInUsd { get; set; }
+
+        [DataMember(Name = "market_cap_euro")]
+        public string MarketCapInEuro { get; set; }
+
+        [DataMember(Name = "available_supply")]
+        public string AvailableSupply { get; set; }
+
+        [DataMember(Name = "total_supply")]
+        public string TotalSupply { get; set; }
+
+        [DataMember(Name = "percent_change_1h")]
+        public string PercentChangeOneHour { get; set; }
+
+        [DataMember(Name = "percent_change_24h")]
+        public string PercentChangeTwentyFoutHours{ get; set; }
+
+        [DataMember(Name = "percent_change_7d")]
+        public string PercentChangeSevenDays { get; set; }
+
+        [DataMember(Name = "last_updated")]
+        public string LastUpdate { get; set; }
+        
+    }
+}
---
+++ CryptoTracker.WebApi/CryptoTracker.DataAccess/Repositories/HttpJsonRepository.cs
@@ -0,0 +1,42 @@
+﻿using CryptoTracker.DataAccess.Models;
+using CryptoTracker.Exceptions;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CryptoTracker.DataAccess.Repositories
+{
+    public class HttpJsonRepository
+    {
+        private string _baseAddress;
+        public HttpJsonRepository(string baseAddress) {
+            _baseAddress = baseAddress;
+        }
+        public async Task<List<ExchangeModel>> GetMany(string api, List<string> queryParams) {
+            List<ExchangeModel> result = new List<ExchangeModel>();
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    client.BaseAddress = new Uri(_baseAddress);
+                    MediaTypeWithQualityHeaderValue contentType = new MediaTypeWithQualityHeaderValue("application/json");
+                    client.DefaultRequestHeaders.Accept.Add(contentType);
+                    HttpResponseMessage response = client.GetAsync(api).Result;
+                    var stringData = await response.Content.ReadAsStringAsync();
+                    result = JsonConvert.DeserializeObject<List<ExchangeModel>>(stringData);
+                }
+                return result;
+            }
+            catch (Exception ex) {
+                ExceptionHandler.HandleRepositoryException(ex);
+            }
+            return result;
+        }
+
+        
+    }
+}
